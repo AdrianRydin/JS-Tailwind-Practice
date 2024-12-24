10512a8be4cc0dbafbba4f88f106fe0a821148dc
AdrianRydin <adrian.rydin@gmail.com>
2024-12-24 16:19:43 +0100

    added header component and added Inter font

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import "./header.css";
+
+function Header() {
+    return ( 
+        <header className="flex justify-space-between items-center font-sans Header">
+            <h1 className="Header__title">Adrian</h1>
+        </header>
+     );
+}
+
+export default Header;
--- src/pages/pageNotFound.jsx
+++ src/pages/pageNotFound.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import "./pageNotFound.css";
+
 
 export default function PageNotFound() {
     return (
-        <div className="flex flex-col gap-4 h-screen font-xl ">
-        <h1>Page Not Found</h1>
-        <Link to={"/"}>Go to Home</Link>
-        </div>
+        <section className="flex flex-col items-center gap-4 page-not-found">
+        <h1 className="text-3xl">Page Not Found</h1>
+        <Link to={"/"} className="text-xl home-btn">Go to Home</Link>
+        </section>
     );
     }
